8fe89787d170b60c24eb4e496bc1814369e8a3a7
Levin Jian <jianzhirong@gmail.com>
2015-06-24 10:03:20 +0800

    camke step 1

--- BuildSystem/CMake/Step1/tutorial.cpp
+++
@@ -1,9 +0,0 @@
-// A simple program that computes the square root of a number
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-int main (int argc, char *argv[])
-{
-  printf("hello, cmake\n");
-  return 0;
-}
