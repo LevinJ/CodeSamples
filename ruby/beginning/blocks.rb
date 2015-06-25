070a1a8170e588125f5afe1b29b891f0c873244f
Levin Jian <jianzhirong@gmail.com>
2015-06-25 17:15:54 +0800

    ruby blocks

--- ruby/beginning/blocks.rb
+++ ruby/beginning/blocks.rb
@@ -1,2 +1,28 @@
-class Blocks
+def test
+   puts "You are in the method"
+   yield
+   puts "You are again back to the method"
+   yield
 end
+test {puts "You are in the block"}
+
+
+def test
+   yield 5
+   puts "You are in the method test"
+   yield 100
+end
+test {|i| puts "You are in the block #{i}"}
+  
+  
+BEGIN { 
+  # BEGIN block code 
+  puts "BEGIN code block"
+} 
+
+END { 
+  # END block code 
+  puts "END code block"
+}
+  # MAIN block code 
+puts "MAIN code block"
