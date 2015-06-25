f956412f7a6f861cce8c94905a706c7504cca308
Levin Jian <jianzhirong@gmail.com>
2015-06-25 17:34:08 +0800

    ruby modules

---
+++ ruby/beginning/Trig.rb
@@ -0,0 +1,20 @@
+module Trig
+   PI = 3.141592654
+   def Trig.sin(x)
+   # ..
+     puts "sin from trig"
+   end
+   def Trig.cos(x)
+   # ..
+   end
+end
+
+
+module Moral
+   VERY_BAD = 0
+   BAD = 1
+   def Moral.sin(badness)
+   # ...
+     puts "sin from Moral"
+   end
+end
---
+++ ruby/beginning/modules.rb
@@ -0,0 +1,5 @@
+$LOAD_PATH << '.'
+require 'Trig'
+
+Trig.sin(Trig::PI/4)
+Moral.sin(0)
