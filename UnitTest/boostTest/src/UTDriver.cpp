ca22ec200ecf2840fc73e778bd77ec057a1e4cbe
Levin Jian <jianzhirong@gmail.com>
2015-07-15 17:15:19 +0800

    boost test-add the how to run the test comment

--- UnitTest/boostTest/src/UTDriver.cpp
+++ UnitTest/boostTest/src/UTDriver.cpp
@@ -1,3 +1,5 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE MyTest
 #include <boost/test/unit_test.hpp>
+
+//Entry file for unit test, run ./main  --log_level=all under build directory to do the actual tesing
