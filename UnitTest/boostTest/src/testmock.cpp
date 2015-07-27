e3242e9bedf3509d6f2e677a1eee39479d1f75d8
Levin Jian <jianzhirong@gmail.com>
2015-07-27 09:37:57 +0800

    add boost test mock

---
+++ UnitTest/boostTest/src/testmock.cpp
@@ -0,0 +1,35 @@
+
+#define BOOST_TEST_MAIN
+
+
+#include <boost/test/unit_test.hpp>
+#include <turtle/mock.hpp>
+
+
+class view
+{
+public:
+    virtual void display( int result ) = 0;
+};
+
+class calculator
+{
+public:
+    calculator( view& v );
+
+    void add( int a, int b ); // the result will be sent to the view 'v'
+};
+
+
+MOCK_BASE_CLASS( mock_view, view ) // declare a 'mock_view' class implementing 'view'
+{
+    MOCK_METHOD( display, 1 )      // implement the 'display' method from 'view' (taking 1 argument)
+};
+
+BOOST_AUTO_TEST_CASE( zero_plus_zero_is_zero )
+{
+    mock_view v;
+    calculator c( v );
+    MOCK_EXPECT( v.display ).once().with( 0 ); // expect the 'display' method to be called once (and only once) with a parameter value equal to 0
+    c.add( 0, 0 );
+}
