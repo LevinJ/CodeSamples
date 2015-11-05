a3e5c19b89b7efd3e5fb7c364ae1c48114ca910b
Levin Jian <jianzhirong@gmail.com>
2015-11-05 15:00:05 +0800

    valgrind....access freed object

--- valgrind/hello.cpp
+++ valgrind/hello.cpp
@@ -1,16 +1,54 @@
- #include <stdlib.h>
+#include <stdlib.h>
 #include <stdio.h>
-
-  void f(void)
+  class TestClass2
   {
-     int* x = (int *)malloc(10 * sizeof(int));
-     x[10] = 0;        // problem 1: heap block overrun
-  }                    // problem 2: memory leak -- x not freed
+    int *m_arr;
+  public:
+    TestClass2(){
+      m_arr = new int(1000);
+    };
+    ~TestClass2(){};
+    
+  };
+  class TestClass
+  {
+  public:
+    int m_i;
+    bool m_b;
+    TestClass2 *m_pi;
+    TestClass(){
+      m_i = 1000;
+      m_b = true;
+      m_pi = new TestClass2();
+    };
+    void printit(){
+      printf("We should not see this line...\n");
+    }
+    void Leakit(){
+      TestClass2 *p = new TestClass2();
+    }
+    ~TestClass(){};
+    
+  };
 
+  void MemoryLeak(){
+    TestClass *obj1 = new TestClass();
+  }
+//Valgrind can detect  variable access of freed object
+//Valgrind can not detect method access of freed object
+  void AccessFreedObject(){
+    TestClass *pObj = new TestClass();
+    printf("vow %d  \n", pObj->m_i);
+    delete pObj;
+    pObj->printit();
+    printf("vow %d  \n", pObj->m_i);
+    pObj->printit();
+  }
   int main(void)
   {
     printf("Here we start the call...\n");
-    f();
-    printf("Here we end the call22...\n");
+    MemoryLeak();
+    //AccessFreedObject();
+    printf("Here we end the call...\n");
     return 0;
   }
