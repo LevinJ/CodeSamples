ac766f0c5171fbab50a4dd7963a5eae1491e2ed2
Levin Jian <jianzhirong@gmail.com>
2016-05-06 17:21:00 +0800

    handle tage with / character

--- python/visualizeStateMachine/parsestatemachine.py
+++ python/visualizeStateMachine/parsestatemachine.py
@@ -26,20 +26,20 @@ class ParseStateMachine(LogParsing):
 #     def run(self):
 #         line = '16:34:35.487.0,Fsm,,DepositFsm,INFO1,L0103 TID0 6712 entry state= TerminateData  '
 #         print self.processEntry(line)
-#         line = '16:34:35.486.0,Fsm,,DepositFsm,INFO1,L0209 TID0 6711 exit state= Finalize 16:36:22.744 '
+#         line = '16:34:33.309.0,Fsm,,DepositFsm,INFO1,L0209 TID0 6644 exit state= WaitForClean/PollWithPauses/Poll 16:36:20.723 '
 #         print self.processExit(line)
 #         return
     def processEntry(self, line):
-        m = re.search(r'(.*),,(.*),INFO1(.*)entry state= ([a-zA-Z0-9]*)(.*)', line)
+        m = re.search(r'(.*),,(.*),INFO1(.*)entry state= ([a-zA-Z0-9\/]*)(.*)', line)
         if not m:
             raise Exception("invalid line {}".format(line))
-        return 'entry', m.group(4), m.group(2)
+        return 'entry',   m.group(4).replace('/','.'), m.group(2)
     
     def processExit(self, line):
-        m = re.search(r'(.*),,(.*),INFO1(.*)exit state= ([a-zA-Z0-9]*)(.*)', line)
+        m = re.search(r'(.*),,(.*),INFO1(.*)exit state= ([a-zA-Z0-9\/]*)(.*)', line)
         if not m:
             raise Exception("invalid line {}".format(line))
-        return 'exit', m.group(4), m.group(2)
+        return 'exit',   m.group(4).replace('/','.'), m.group(2)
     
     
     
