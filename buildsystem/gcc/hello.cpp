0f2089fc405706f3b3f54e39bc6780b75362294f
Levin Jian <jianzhirong@gmail.com>
2016-09-05 15:38:40 +0800

    g++ tool usage sample

---
+++ buildsystem/gcc/hello.cpp
@@ -0,0 +1,9 @@
+// hello.cpp
+//https://www3.ntu.edu.sg/home/ehchua/programming/cpp/gcc_make.html
+#include <iostream>
+using namespace std;
+ 
+int main() {
+   cout << "Hello, world!" << endl;
+   return 0;
+}
