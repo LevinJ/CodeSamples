574297cedbd652f564773fde1e6c2c6b474726df
Levin Jian <jianzhirong@gmail.com>
2017-10-18 13:11:19 +0800

    c++, move semantic

--- cplusplus/move_semantic/src/main.cpp
+++ cplusplus/move_semantic/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <algorithm>
+#include <memory>
+using namespace std;
 
 class A
 {
@@ -18,79 +20,84 @@ public:
 	{
 		std::cout << "~A(). length = " << mLength << ".";
 
-		if (mData != NULL) {
-			std::cout << " Deleting resource.";
-			delete[] mData;  // Delete the resource.
-		}
+//		if (mData != NULL) {
+//			std::cout << " Deleting resource.";
+//			delete[] mData;  // Delete the resource.
+//		}
 
 		std::cout << std::endl;
 	}
 
-	// Copy constructor.
-	A(const A& other)
-	: mLength(other.mLength), mData(new int[other.mLength])
-	{
-		std::cout << "A(const A&). length = "
-				<< other.mLength << ". Copying resource." << std::endl;
+//	// Copy constructor.
+//	A(const A& other)
+//	: mLength(other.mLength), mData(new int[other.mLength])
+//	{
+//		std::cout << "A(const A&). length = "
+//				<< other.mLength << ". Copying resource." << std::endl;
+//
+//		std::copy(other.mData, other.mData + mLength, mData);
+//	}
+//
+//	// Copy assignment operator.
+//	A& operator=(const A& other)
+//	{
+//		std::cout << "operator=(const A&). length = "
+//				<< other.mLength << ". Copying resource." << std::endl;
+//
+//		if (this != &other) {
+//			delete[] mData;  // Free the existing resource.
+//			mLength = other.mLength;
+//			mData = new int[mLength];
+//			std::copy(other.mData, other.mData + mLength, mData);
+//		}
+//		return *this;
+//	}
+//
+//	// Move constructor.
+//	A(A&& other)
+//	{
+//		std::cout << "A(A&&). length = "
+//				<< other.mLength << ". Moving resource.\n";
+//
+//		// Copy the data pointer and its length from the
+//		// source object.
+//		mData = other.mData;
+//		mLength = other.mLength;
+//
+//		// Release the data pointer from the source object so that
+//		// the destructor does not free the memory multiple times.
+//		other.mData = NULL;
+//		other.mLength = 0;
+//	}
+//
+//
+//	// Move assignment operator.
+//	A& operator=(A&& other)
+//	{
+//		std::cout << "operator=(A&&). length = "
+//				<< other.mLength << "." << std::endl;
+//
+//		if (this != &other) {
+//			// Free the existing resource.
+//			delete[] mData;
+//
+//			// Copy the data pointer and its length from the
+//			// source object.
+//			mData = other.mData;
+//			mLength = other.mLength;
+//
+//			// Release the data pointer from the source object so that
+//			// the destructor does not free the memory multiple times.
+//			other.mData = NULL;
+//			other.mLength = 0;
+//		}
+//		return *this;
+//	}
 
-		std::copy(other.mData, other.mData + mLength, mData);
+	void disp(){
+		mvar = 100;
+		std::cout<<"mvar "<< mvar<<std::endl;
 	}
-
-	// Copy assignment operator.
-	A& operator=(const A& other)
-	{
-		std::cout << "operator=(const A&). length = "
-				<< other.mLength << ". Copying resource." << std::endl;
-
-		if (this != &other) {
-			delete[] mData;  // Free the existing resource.
-			mLength = other.mLength;
-			mData = new int[mLength];
-			std::copy(other.mData, other.mData + mLength, mData);
-		}
-		return *this;
-	}
-
-	// Move constructor.
-	A(A&& other) : mData(NULL), mLength(0)
-	{
-		std::cout << "A(A&&). length = "
-				<< other.mLength << ". Moving resource.\n";
-
-		// Copy the data pointer and its length from the
-		// source object.
-		mData = other.mData;
-		mLength = other.mLength;
-
-		// Release the data pointer from the source object so that
-		// the destructor does not free the memory multiple times.
-		other.mData = NULL;
-		other.mLength = 0;
-	}
-
-	// Move assignment operator.
-	A& operator=(A&& other)
-	{
-		std::cout << "operator=(A&&). length = "
-				<< other.mLength << "." << std::endl;
-
-		if (this != &other) {
-			// Free the existing resource.
-			delete[] mData;
-
-			// Copy the data pointer and its length from the
-			// source object.
-			mData = other.mData;
-			mLength = other.mLength;
-
-			// Release the data pointer from the source object so that
-			// the destructor does not free the memory multiple times.
-			other.mData = NULL;
-			other.mLength = 0;
-		}
-		return *this;
-	}
-
 	// Retrieves the length of the data resource.
 	size_t Length() const
 	{
@@ -98,8 +105,9 @@ public:
 	}
 
 private:
+	int mvar;
 	size_t mLength; // The length of the resource.
-	int* mData;     // The resource.
+	std::shared_ptr<int>  mData;     // The resource.
 };
 
 #include <vector>
@@ -111,10 +119,11 @@ A get_aobj(){
 
 int main()
 {
-	A temp = get_aobj();
+//	A temp = get_aobj();
+//	temp.disp();
 	// Create a vector object and add a few elements to it.
-//	std::vector<A> v;
-//	v.push_back(A(25));
+	std::vector<A> v;
+	v.push_back(A(25));
 
 //	std::vector<A> v2;
 //	v2 = std::move(v);
