fe8593685b90fa41d155d0dbb3c48a29699a3c72
Levin Jian <jianzhirong@gmail.com>
2021-04-08 13:05:27 +0800

    test proto

--- lcm/src/test_recv_proto.py
+++ lcm/src/test_recv_proto.py
@@ -6,6 +6,8 @@ import numpy as np
 from exlcm import point2d_list_t
 import traceback
 from  enginetargetlanes_pb2 import EngineTargetLanes
+from entity_pb2 import pb_p_arrow3d
+from entity_pb2 import pb_p_mutipoint
 
 
 class TestLcmLog(object):
@@ -15,8 +17,12 @@ class TestLcmLog(object):
         self.lc = lcm.LCM("file:///media/levin/DATADRIVE1/mapmatch/2019_11_01_17_05_27.log?mode={}&speed=15".format(self.mode))
         
         self.map_obj = EngineTargetLanes()
+        self.loc_obj = pb_p_arrow3d()
+        self.melane_obj = pb_p_mutipoint()
         self.channel_handler_list = []
         self.channel_handler_list.append(("PROTO_EngineTargetLanes", self.map_obj))
+        self.channel_handler_list.append(("Localization", self.loc_obj))
+        self.channel_handler_list.append(("LocFeature_melane_grid", self.melane_obj))
         return
     def run(self):
         if self.mode == "w":
@@ -25,12 +31,13 @@ class TestLcmLog(object):
             self.recv()
         return
     def lcm_handler(self, channel, data):
-        print("channel = {}".format(channel))
+       
         for sub, proto_obj in self.channel_handler_list:
             if sub != channel:
                 continue
             proto_obj.ParseFromString(data)
-            
+            print("channel = {}, timestamp={:.3f}".format(channel, proto_obj.header.timestamp_sec))
+            # print(proto_obj)   
         return
     
     
