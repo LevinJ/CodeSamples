33425d466198fd21ff564f7dce9eb26f7f31a572
Levin Jian <jianzhirong@gmail.com>
2023-03-13 19:59:44 +0800

    add mat normalization api

--- imageprocessing/opencvsample/intro/mat_vec.cpp
+++ imageprocessing/opencvsample/intro/mat_vec.cpp
@@ -4,32 +4,26 @@
 #include <vector>
 using namespace cv;
 
-int main(int argc, char** argv )
-{
-    
-    cv::Mat m(2,3,CV_8UC3, cv::Scalar(40,80,255)); 
+void normanize(cv::Mat &m, float scale, cv::Scalar &mean, cv::Scalar &variance){
     m.convertTo(m, CV_32FC3);
     std::cout<<m<<std::endl<<std::endl;
-    float scale = 1/255.0;
     
     m *= scale;
     std::cout<<m<<std::endl<<std::endl;
-    // Mat dst;
-    // cv::subtract(m, mean, dst);
 
-        
-    
-    // std::vector<double> mean = {80, 80, 80};
-    // cv::Scalar std(3,  2, 1);
-    // std::cout<<m <<std::endl;
-    // std::cout<<std::endl;
-
-    cv::Scalar mean(0.1, 0.2, 1);
     cv::subtract(m, mean, m);
-    // m = m.mul(Scalar(2,3,4));
+    std::cout<<m<<std::endl<<std::endl;
 
+    cv::divide(m, variance, m);
+    std::cout<<m<<std::endl<<std::endl;
+}
+
+int main(int argc, char** argv )
+{
+    cv::Mat m(2,3,CV_8UC3, cv::Scalar(40,80,255)); 
+    float scale = 1/255.0;
+    cv::Scalar mean(0.1, 0.2, 1);
     cv::Scalar variance(10, 2, 1);
 
-    std::cout<<m<<std::endl;
-
+    normanize(m, scale, mean, variance);  
 }
